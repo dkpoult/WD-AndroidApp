d7e4f19e77bd9f2ee1444ca4c6229bee619e7a24
Darren <DarrenBlackwell5@gmail.com>
2019-02-28 23:59:59 +0200

    Added some network handling -volley

--- app/src/main/java/com/example/witsdaily/CourseRegistration.java
+++ app/src/main/java/com/example/witsdaily/CourseRegistration.java
@@ -1,9 +1,14 @@
 package com.example.witsdaily;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CourseRegistration extends AppCompatActivity {
+    TextView tvState = (TextView) findViewById(R.id.tvState);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,17 +16,37 @@ public class CourseRegistration extends AppCompatActivity {
         setContentView(R.layout.activity_course_registration);
     }
 
+    public void clickRegistration(View v) {
+        //when the user clicks the button this is the code that creates the course
+        if (!validFields()) {
 
-    /*Give: 'course_code' - 8 character code. 'course_name' - Any name.
-    'course_description' - Arbitrary description.
-    Returns: 'response_code' - Can be:
+            return;
+        } else {
 
-    "successful"
-    "failed_already_xists"
-    "failed_no_perm"
-    "failed_missing_param"
-    "failed_invalid_param"
-    "failed_unknown"
+        }
+    }
 
-    */
-}
+    private boolean validFields() {
+        // prevents sql injection, makes sure that passwords match etc
+        tvState.setTextColor(Color.RED);
+        tvState.setText("Failed:"); // this should specify what is wrong
+        return true;
+    }
+
+    private void handleResponse(String response) { // handle response once the volley script is complete
+        tvState.setTextColor(Color.RED); // it wont display if successful
+        String stateText = "";
+        switch (response){
+            case "successful": /*do something*/;
+                Toast.makeText(CourseRegistration.this, "Successfully created new course," +
+                        ""      , Toast.LENGTH_LONG).show();;return;
+            case "failed_already_exists": stateText = "Course already exists";break;
+            case "failed_no_perm": stateText = "No permissions to add course";break;
+            case "failed_missing_param": stateText = "Missing parameters";break;
+            case "failed_invalid_param": stateText = "Invalid field";break;
+            case "failed_unknown": stateText = "Unknown";break;
+        }
+        Toast.makeText(CourseRegistration.this, "Failed To Create Course" , Toast.LENGTH_LONG).show();
+        tvState.setText("Failed: "+stateText);
+    }
+    }
