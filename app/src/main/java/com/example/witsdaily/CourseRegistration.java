81621b896cb20dc7d4f5f18c406fb328bf5efda8
Darren <DarrenBlackwell5@gmail.com>
2019-02-28 23:40:07 +0200

    Created and designed the course registration page

---
+++ app/src/main/java/com/example/witsdaily/CourseRegistration.java
@@ -0,0 +1,13 @@
+package com.example.witsdaily;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_registration);
+    }
+}
