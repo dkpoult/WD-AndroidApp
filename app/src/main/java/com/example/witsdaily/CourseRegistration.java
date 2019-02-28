b0f9ec80567efdb1281c3cfb748b031686c9643a
Darren <DarrenBlackwell5@gmail.com>
2019-02-28 23:43:04 +0200

    Ensured course description is 8 chars only

--- app/src/main/java/com/example/witsdaily/CourseRegistration.java
+++ app/src/main/java/com/example/witsdaily/CourseRegistration.java
@@ -10,4 +10,18 @@ public class CourseRegistration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_registration);
     }
+
+
+    /*Give: 'course_code' - 8 character code. 'course_name' - Any name.
+    'course_description' - Arbitrary description.
+    Returns: 'response_code' - Can be:
+
+    "successful"
+    "failed_already_xists"
+    "failed_no_perm"
+    "failed_missing_param"
+    "failed_invalid_param"
+    "failed_unknown"
+
+    */
 }
